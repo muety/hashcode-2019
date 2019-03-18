9ef4dc43c54314a7d2a5381827d590bcdee2aebc
Ferdinand Mütsch <mail@ferdinand-muetsch.de>
2019-03-18 01:44:54 +0100

    Replace naively-trimmed greedy solution with closest-tag-length approach.

--- src/App.scala
+++ src/App.scala
@@ -1,15 +1,8 @@
 object App extends App {
-  val solution = new GreedySolution
+  val solution = new GreedySolutionTrimmed
   solution.run("a")
   solution.run("b")
   solution.run("c")
   solution.run("d")
   solution.run("e")
-
-//  val solution = new GreedySolutionTrimmed(10000, 0.5f)
-//  solution.run("a")
-//  solution.run("b")
-//  solution.run("c")
-//  solution.run("d")
-//  solution.run("e")
 }
--- src/GreedySolutionTrimmed.scala
+++ src/GreedySolutionTrimmed.scala
@@ -1,13 +1,57 @@
-class GreedySolutionTrimmed(trimThreshold: Int, sampleSize: Float) extends GreedySolution {
+class GreedySolutionTrimmed(trimThreshold: Int = 10000, tagSizeTolerance: Int = 2, fallbackConsiderRatio: Float = .2f) extends Solution {
+  var hSlides: List[Slide] = List()
+  var vSlides: List[Slide] = List()
+  var hPicList: List[Picture] = List()
+  var vPicList: List[Picture] = List()
+  var usedSlides: Set[Slide] = Set()
+
   override def execute = {
-    if (n > trimThreshold) {
-      hPics = hPics.par.toList.sortBy(_.tags.size).reverse.take(math.round(hPics.size * sampleSize)).toSet
-      vPics = vPics.par.toList.sortBy(_.tags.size).reverse.take(roundEven(vPics.size * sampleSize)).toSet
-      updateN
+    val cleanUpCooldown = 1000
+    val slides = Array.ofDim[Slide](n)
+
+    time {
+      hPicList ++= hPics.par.toList.sortBy(_.tags.size).reverse
+      vPicList ++= vPics.par.toList.sortBy(_.tags.size).reverse
+
+      hSlides ++= hPicList.map(p => Slide(pictures = Set(p)))
+      vSlides ++= vPicList.sliding(2, 2).map(pl => Slide(pictures = pl.toSet))
+
+      slides(0) = hSlides.headOption.getOrElse(vSlides.head)
+      usedSlides += slides.head
+
+      for (i <- 0 until n - 1) {
+        if ((i + 1) % 10 == 1) println(s"${i + 1} / ${n - 1}")
+        val slide = findBestMatch(slides(i))
+        slides(i + 1) = slide
+        usedSlides += slide
+
+        if (i % cleanUpCooldown == 0) cleanUp
+      }
     }
-    super.execute
-    // TODO: Append remaining pictures in random order to the end
+    slides
   }
 
-  def roundEven(d: Double): Int = math.round(d / 2) * 2 toInt
+  def findBestMatch(slide: Slide): Slide = {
+    val range = slide.tags.size - tagSizeTolerance to slide.tags.size + tagSizeTolerance toSet
+    val hConsider = hSlides.par.filter(!usedSlides.contains(_)) match {
+      case s if s.isEmpty => List()
+      case s if s.size < trimThreshold => s
+      case s if s.exists(range contains _.tags.size) => s.filter(range contains _.tags.size)
+      case s => s.toList.sortBy(_.tags.size - slide.tags.size).take(math.ceil(s.size * fallbackConsiderRatio).toInt)
+    }
+    val vConsider = vSlides.par.filter(!usedSlides.contains(_)) match {
+      case s if s.isEmpty => List()
+      case s if s.size < trimThreshold => s
+      case s if s.exists(range contains _.tags.size) => s.filter(range contains _.tags.size)
+      case s => s.toList.sortBy(_.tags.size - slide.tags.size).take(math.ceil(s.size * fallbackConsiderRatio).toInt)
+    }
+    val hMax = if (hConsider.nonEmpty) hConsider.par.maxBy(_.score(slide)) else null
+    val vMax = if (vConsider.nonEmpty) vConsider.par.maxBy(_.score(slide)) else null
+    Set(hMax, vMax).filter(_ != null).maxBy(_.score(slide))
+  }
+
+  def cleanUp = {
+    hSlides = hSlides.par.filter(!usedSlides.contains(_)).toList
+    vSlides = vSlides.par.filter(!usedSlides.contains(_)).toList
+  }
 }
